30a8c4dc973f9dffc140cc355fb1ac729e8580bd
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2024-01-05 14:34:42 -0600

    Add axios library and implement fetching users asynchronously

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,18 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [ 
-    { id: '0', name: 'Dude Lebowski' }, 
-    { id: '1', name: 'Neil Young' }, 
-    { id: '2', name: 'Dave Gray' }
-    
-]
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
-const usersSlice = createSlice({ 
-    name: 'users' ,
-    initialState, 
-    reducers: {} 
-})
+const initialState = [];
 
-export const selectAllUsers = (state) => state.users; 
+export const fetchUsers = createAsyncThunk("posts/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return [...response.data];
+  } catch (err) {
+    return err.message;
+  }
+});
 
-export default usersSlice.reducer
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers(builder) { 
+    builder.addCase(fetchUsers.fulfilled, (state, action) => { 
+        return action.payload
+    })
+  }
+});
+
+export const selectAllUsers = (state) => state.users;
+
+export default usersSlice.reducer;
